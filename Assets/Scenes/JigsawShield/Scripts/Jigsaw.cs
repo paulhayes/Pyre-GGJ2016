6866dcdb7c98132bd02995927c4a66de3656507a
Paul Hayes <paul.hayes@entropedia.co.uk>
2016-01-31 09:57:01 +0000

    Finished Jigsaw

--- Assets/Scenes/JigsawShield/Scripts/Jigsaw.cs
+++ Assets/Scenes/JigsawShield/Scripts/Jigsaw.cs
@@ -1,25 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class Jigsaw : MonoBehaviour {
 
     public Collider[] pieces;
-    public Transform depthPosition;
     public float zMovement;
-
+    public BoxCollider dragPlane;
+    public float snapPosition;
     Collider currentlyDragging;
     bool isDragging;
+    Vector3 startPos;
+    Vector3 dragPosDiff;
+
 
     void Start () {
 	
 	}
 	
 	void Update () {
+        RaycastHit hit;
 	    if( Input.GetButtonDown("Fire1") ){
-            RaycastHit hit;
             int index;
             if( Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 2f ) && ((index=System.Array.IndexOf( pieces,hit.collider)) != -1 ) ){
-                StartDrag( pieces[index] );
+                StartDrag( pieces[index], hit );
             }
 
         }
@@ -28,24 +32,57 @@ public class Jigsaw : MonoBehaviour {
         }
 
         if( isDragging ) {
-            
+            if( dragPlane.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 10f) ){
+                currentlyDragging.transform.position = (hit.point) + dragPosDiff;
+
+            }
         }
 	}
 
-    void StartDrag(Collider piece){
+    void StartDrag(Collider piece, RaycastHit hit){
         currentlyDragging = piece;
         Vector3 p = currentlyDragging.transform.position;
         p += Camera.main.transform.forward * zMovement;
         isDragging = true;
-        
+        dragPosDiff = currentlyDragging.transform.position - hit.point;
+        startPos = hit.point;        
     }
 
     void StopDrag(){
         if( currentlyDragging != null ){
             Vector3 p = currentlyDragging.transform.position;
             p -= Camera.main.transform.forward * zMovement;
+            currentlyDragging.transform.position = p;
+            Snap();
             currentlyDragging = null;
+            Check();
         }
         isDragging = false;
+
     }
+
+    void Snap(){
+        foreach( var piece in pieces ){
+            if( Vector3.Distance( piece.transform.position,currentlyDragging.transform.position ) < snapPosition ){
+                currentlyDragging.transform.position = piece.transform.position;
+
+            } 
+        }
+    }
+
+    void Check(){
+        Vector3 pos = pieces[0].transform.position;
+        bool isAllDone = pieces.Where(p=>p.transform.position==pos).Count() == pieces.Length;
+        if( isAllDone ) {
+            Debug.Log("Done");
+            Invoke("OnComplete",2f);
+        }
+
+    }
+
+    void OnComplete(){
+        SendMessage("RitualComplete");
+    }
+
+
 }
