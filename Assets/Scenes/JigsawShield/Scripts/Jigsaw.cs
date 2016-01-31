34cf8c8c695aa4eb6b565b82bdad183f2f281990
Paul Hayes <paul.hayes@entropedia.co.uk>
2016-01-31 03:09:47 +0000

    Jigsaw game work

---
+++ Assets/Scenes/JigsawShield/Scripts/Jigsaw.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jigsaw : MonoBehaviour {
+
+    public Collider[] pieces;
+    public Transform depthPosition;
+    public float zMovement;
+
+    Collider currentlyDragging;
+    bool isDragging;
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	    if( Input.GetButtonDown("Fire1") ){
+            RaycastHit hit;
+            int index;
+            if( Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 2f ) && ((index=System.Array.IndexOf( pieces,hit.collider)) != -1 ) ){
+                StartDrag( pieces[index] );
+            }
+
+        }
+        if( Input.GetButtonUp("Fire1") ){
+            StopDrag();
+        }
+
+        if( isDragging ) {
+            
+        }
+	}
+
+    void StartDrag(Collider piece){
+        currentlyDragging = piece;
+        Vector3 p = currentlyDragging.transform.position;
+        p += Camera.main.transform.forward * zMovement;
+        isDragging = true;
+        
+    }
+
+    void StopDrag(){
+        if( currentlyDragging != null ){
+            Vector3 p = currentlyDragging.transform.position;
+            p -= Camera.main.transform.forward * zMovement;
+            currentlyDragging = null;
+        }
+        isDragging = false;
+    }
+}
