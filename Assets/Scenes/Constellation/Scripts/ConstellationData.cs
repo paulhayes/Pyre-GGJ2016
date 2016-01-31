9c94d0a5c9e08ee9b73aaacd2748f6430cf23888
denisgruba <denisgruba@gmail.com>
2016-01-31 02:06:13 +0000

    Revert "Revert "Moved things arround. More work onconstellation theme""
    This reverts commit 92e8c7186cab0eb3b590edcef65d2b8b8e615268.

---
+++ Assets/Scenes/Constellation/Scripts/ConstellationData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu()]
+public class ConstellationData : ScriptableObject {
+
+    public GameObject marble;
+    public int[] holdIndexes;
+
+}
