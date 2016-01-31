972af13f231324b3419fec04b16e3aba193b631a
Paul Hayes <paul.hayes@entropedia.co.uk>
2016-01-31 01:35:52 +0000

    Moved things arround. More work onconstellation theme

--- Assets/Scenes/Constellation/Scripts/Constellation.cs
+++ Assets/Scenes/Constellation/Scripts/Constellation.cs
@@ -1,19 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Constellation : MonoBehaviour {
 
+    public Transform[] holes;
+    public ConstellationData gameData;
+    public Rigidbody[] marbles;
+    public int marbleCount;
+    public Transform marbleStartPosition;
+     
     ReturnToHub returnToHub;
+
+    List<int> filledHoles = new List<int>();
 
 	void Start () {
         returnToHub = FindObjectOfType<ReturnToHub>();
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+
+
 	}
 	
 	void Update () {
-	
+	    if( Input.GetButton("Fire1") ){
+            RaycastHit hit;
+            if( Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 2f)){
+                int index = System.Array.IndexOf(holes,hit.transform);
+                if( index != -1 ){
+                    int filledHoldIndex = filledHoles.IndexOf(index);
+                    if( filledHoldIndex==-1 ){
+                        Add(index);
+                    }
+                    else{
+                        Remove(index);
+                    }
+
+                }
+            }
+        }
 	}
+
+    void Add(int index){
+        
+        //holes[index]
+        //filledHoles.Add(index);
+
+    }
+
+    void Remove(int index){
+        //filledHoles.Remove(index);
+
+    }
 
 
 }
